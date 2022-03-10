fc2c34b55bd6556fcd6eb8c69a84cb6fda25a33c
Bonvic <bundi.bonvic@gmail.com>
2022-03-10 16:05:31 +0300

    firebase cert url

--- client/utils/firestore.ts
+++ client/utils/firestore.ts
@@ -16,7 +16,7 @@ if (!admin.apps.length) {
       auth_uri: 'https://accounts.google.com/o/oauth2/auth',
       token_uri: 'https://oauth2.googleapis.com/token',
       auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-      client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7l7g0%40findy-80b8f.iam.gserviceaccount.com',
+      client_x509_cert_url: process.env.FIREBASE_CERT_URL,
     } as any),
   });
 }
