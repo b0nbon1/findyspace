820040d3697324460e9c8c38df94aabf7a2594a6
Bonvic <bundi.bonvic@gmail.com>
2022-03-16 11:19:18 +0300

    add popular activities

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -24,7 +24,7 @@ const Theme = createTheme({
   },
   typography: {
     fontFamily: [
-      '"Outfit"',
+      '"Inter"',
       '-apple-system',
       'sans-serif',
       '"Apple Color Emoji"',
