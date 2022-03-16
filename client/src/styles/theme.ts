5d5f1fae7c65d8bd845aaf9b4abc32d0d44d2056
Bonvic <bundi.bonvic@gmail.com>
2022-03-16 12:05:53 +0300

    add coming soon page text

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,5 +1,37 @@
 import { createTheme } from '@mui/material/styles';
 
-const Theme = createTheme({});
+const Theme = createTheme({
+  palette: {
+    primary: {
+      main: '#5D33D5',
+      light: '#C3C4FE',
+    },
+    background: {
+      default: '#F8FAFC',
+    },
+    secondary: {
+      main: '#F27956',
+    },
+    success: {
+      main: '#2BBF57',
+    },
+    warning: {
+      main: '#F5CE40',
+    },
+    error: {
+      main: '#FF0000',
+    },
+  },
+  typography: {
+    fontFamily: [
+      '"Inter"',
+      '-apple-system',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
 
 export default Theme;
