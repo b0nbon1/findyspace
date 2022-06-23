e2da9d199aec460363c48f2e8832b98cae6c5f9e
Bonvic Bundi <bundi.bonvic@gmail.com>
2022-06-23 13:58:36 +0300

    small fixes on auth (#20)

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -20,7 +20,7 @@ function MyApp(props: any) {
   const {
     Component,
     emotionCache = clientSideEmotionCache,
-    pageProps: { session, ...pageProps },
+    pageProps,
     user,
   } = props;
   return (
